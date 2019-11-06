d08eef9cc90f8846111ac84c96df479ce3f429ff
Otavio Bonder <otaviobonder@gmail.com>
2019-11-06 11:46:01 -0300

    changed loading modal for chart comparision

--- src/components/chartComparision/loadingModal.js
+++ src/components/chartComparision/loadingModal.js
@@ -45,8 +45,11 @@ export default function LoadingModal(props) {
           >
             <GridLoader color="#3e2e56" />
             <Typography align="center" style={{ color: "white" }}>
-              Some simulations can take up to 1 minute. I know that sucks, but
-              I'm gonna improve the algo!
+              Some simulations may take a long time to finish. I'm improving the
+              code to use a local database instead of a free API{" "}
+              <span role="img" aria-label="smile">
+                😄
+              </span>
             </Typography>
           </div>
         </Fade>
